03c730092edf600ed885efc5c072774de44acb91
Kala <vdoletskaya@bk.ru>
2022-09-09 19:57:09 +0300

    3

--- St2/day/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿day = input('введите день недели '
-if day == '7':
-    print ('да')
-elif day == '6':
-    print ('да')
-elif day > '7':
-    print ('введите число меньше')
-else:
-    print ('нет')
